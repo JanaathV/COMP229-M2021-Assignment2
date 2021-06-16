b44842e7bd1c30dcb39de7ea0cbf9576b3b07c65
JanaathV <janaath@live.com>
2021-06-16 17:46:25 -0400

    added dbconfig for MongoDB Atlas

--- .history/Server/Config/db_20210616171502.ts
+++ .history/Server/Config/db_20210616171502.ts
@@ -1,5 +1,3 @@
 export const RemoteURI = "mongodb+srv://admin:TFsuquPMX7MauRie@cluster-comp229.qkxqf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 export const HostName = "RemoteHost";
 export const Secret = "someSecret";
-
-//Setting up of the RemoteHost for MongoDB Atlas
